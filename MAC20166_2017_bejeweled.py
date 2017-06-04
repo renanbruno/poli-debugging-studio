85eefe57d4ed0e23025a80737f6039fe35d8aa6a
renanbruno <renanbruno_delta@usp.br>
2017-06-04 12:57:51 -0300

    Update MAC20166_2017_bejeweled.py

--- MAC20166_2017_bejeweled.py
+++ MAC20166_2017_bejeweled.py
@@ -538,4 +538,448 @@ def testes_unitarios ():
 	print("\nBateria de testes encerrada !")
 	print("\nTestes aprovados : [%d/20]" % testes )
 testes_unitarios()
-#main()
+"""
+  AO PREENCHER ESSE CABEÇALHO COM O MEU NOME E O MEU NÚMERO USP,
+  DECLARO QUE SOU A ÚNICA PESSOA AUTORA E RESPONSÁVEL POR ESSE PROGRAMA.
+  TODAS AS PARTES ORIGINAIS DESSE EXERCÍCIO PROGRAMA (EP) FORAM
+  DESENVOLVIDAS E IMPLEMENTADAS POR MIM SEGUINDO AS INSTRUÇÕES
+  DESSE EP E, PORTANTO, NÃO CONSTITUEM ATO DE DESONESTIDADE ACADÊMICA,
+  FALTA DE ÉTICA OU PLÁGIO.
+  DECLARO TAMBÉM QUE SOU A PESSOA RESPONSÁVEL POR TODAS AS CÓPIAS
+  DESSE PROGRAMA E QUE NÃO DISTRIBUÍ OU FACILITEI A
+  SUA DISTRIBUIÇÃO. ESTOU CIENTE QUE OS CASOS DE PLÁGIO E
+  DESONESTIDADE ACADÊMICA SERÃO TRATADOS SEGUNDO OS CRITÉRIOS
+  DIVULGADOS NA PÁGINA DA DISCIPLINA.
+  ENTENDO QUE EPS SEM ASSINATURA NÃO SERÃO CORRIGIDOS E,
+  AINDA ASSIM, PODERÃO SER PUNIDOS POR DESONESTIDADE ACADÊMICA.
+
+  Nome : Renan Bruno
+  NUSP : 10332980
+  Turma: T6
+  Prof.: Kelly Rosa Braghetto
+
+  Referências: Com exceção das rotinas fornecidas no enunciado
+  e em sala de aula, caso você tenha utilizado alguma referência,
+  liste-as abaixo para que o seu programa não seja considerado
+  plágio ou irregular.
+
+  Exemplo:
+  - O algoritmo Quicksort foi baseado em
+  https://pt.wikipedia.org/wiki/Quicksort
+  http://www.ime.usp.br/~pf/algoritmos/aulas/quick.html
+"""
+# ======================================================================
+#
+#   FUNÇÕES FORNECIDAS: NÃO DEVEM SER MODIFICADAS
+#
+# ======================================================================
+import random
+random.seed(0)
+
+def main():
+    '''
+    Esta é a função principal do seu programa. Ela contém os comandos que
+    obtêm os parâmetros necessários para criação do jogo (número de linhas,
+    colunas e cores), e executa o laço principlal do jogo: ler comando,
+    testar sua validade e executar comando.
+
+    ******************************************************
+    ** IMPORTANTE: ESTA FUNÇÃO NÃO DEVE SER MODIFICADA! **
+    ******************************************************
+    '''
+    print()
+    print("=================================================")
+    print("             Bem-vindo ao Gemas!                 ")
+    print("=================================================")
+    print()
+
+    pontos = 0
+    # lê parâmetros do jogo
+    num_linhas = int(input("Digite o número de linhas [3-10]: ")) # exemplo: 8
+    num_colunas = int(input("Digite o número de colunas [3-10]: ")) # exemplo: 8
+    num_cores = int(input("Digite o número de cores [3-26]: ")) # exemplo: 7
+    # cria tabuleiro com configuração inicial
+    tabuleiro = criar (num_linhas, num_colunas)
+    completar (tabuleiro, num_cores)
+    num_gemas = eliminar (tabuleiro)
+    while num_gemas > 0:
+        deslocar (tabuleiro)
+        completar (tabuleiro, num_cores)
+        num_gemas = eliminar (tabuleiro)
+    # laço principal do jogo
+    while existem_movimentos_validos(tabuleiro) == True: # Enquanto houver movimentos válidos...
+        exibir (tabuleiro)
+        comando = input("Digite um comando (perm, dica, sair ou ajuda): ")
+        if comando == "perm":
+            linha1 = int(input("Digite a linha da primeira gema: "))
+            coluna1 = int(input("Digite a coluna da primeira gema: "))
+            linha2 = int(input("Digite a linha da segunda gema: "))
+            coluna2 = int(input("Digite a coluna da segunda gema: "))
+            print ()
+            valido = trocar ( linha1, coluna1, linha2, coluna2, tabuleiro)
+            if valido:
+                num_gemas = eliminar (tabuleiro)
+                total_gemas = 0
+                while num_gemas > 0:
+                    # Ao destruir gemas, as gemas superiores são deslocadas para "baixo",
+                    # criando a possibilidade de que novas cadeias surjam.
+                    # Devemos então deslocar gemas e destruir cadeias enquanto houverem.
+                    deslocar (tabuleiro)
+                    completar (tabuleiro, num_cores)
+                    total_gemas += num_gemas
+                    print("Nesta rodada: %d gemas destruidas!" % num_gemas )
+                    exibir (tabuleiro)
+                    num_gemas = eliminar (tabuleiro)
+                pontos += total_gemas
+                print ()
+                print ("*** Você destruiu %d gemas! ***" % (total_gemas))
+                print ()
+            else:
+                print ()
+                print ("*** Movimento inválido! ***")
+                print ()
+        elif comando == "dica":
+            pontos -= 1
+            linha, coluna = obter_dica (tabuleiro)
+            print ()
+            print ("*** Dica: Tente permutar a gema na linha %d e coluna %d ***" % (linha, coluna))
+            print ()
+        elif comando == "sair":
+            print ("Fim de jogo!")
+            print ("Você destruiu um total de %d gemas" % (pontos))
+            return
+        elif comando == "ajuda":
+            print("""
+============= Ajuda =====================
+perm:  permuta gemas
+dica:  solicita uma dica (perde 1 ponto)
+sair:  termina o jogo
+=========================================
+                  """)
+        else:
+            print ()
+            print ("*** Comando inválido! Tente ajuda para receber uma lista de comandos válidos. ***")
+            print ()
+    print("*** Fim de Jogo: Não existem mais movimentos válidos! ***")
+    print ("Você destruiu um total de %d gemas" % (pontos))
+
+def completar (tabuleiro, num_cores):
+    ''' (matrix, int) -> None
+
+    Preenche espaços vazios com novas gemas geradas aleatoriamente.
+
+    As gemas são representadas por strings 'A','B','C',..., indicando sua cor.
+    '''
+    alfabeto = ['A','B','C','D','E','F','G','H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    num_linhas = len (tabuleiro)
+    num_colunas = len (tabuleiro[0])
+    for i in range (num_linhas):
+        for j in range (num_colunas):
+            if tabuleiro[i][j] == ' ':
+                gema = random.randrange (num_cores)
+                tabuleiro[i][j] = alfabeto[gema]
+
+
+# ======================================================================
+#
+#   FUNÇÕES A SEREM IMPLEMENTADAS POR VOCÊ
+#
+# ======================================================================
+
+def criar (num_linhas, num_colunas):
+    ''' (int,int) -> matrix
+
+    Cria matriz de representação do tabuleiro e a preenche com
+    espaços vazios representados por ' '.
+
+    Retorna a matriz criada.
+    '''
+    matriz = []
+
+    for i in range(num_linhas):
+        linha = []
+        for j in range(num_colunas):
+            linha.append(' ')
+            
+        matriz.append(linha)
+    
+    return matriz
+
+def exibir (tabuleiro):
+    ''' (matrix) -> None
+
+    Exibe o tabuleiro.
+    '''
+    num_linhas  = len(tabuleiro)
+    num_colunas = len(tabuleiro[0])
+    
+    print ("    ", end="")
+    for x in range(len(tabuleiro[0])):
+        print (x, end=" ")
+    print ("", end="\n")
+    print ("  ", end="")
+    print ("+", end="-")    
+    for y in range(len(tabuleiro[0])):
+        print ("-", end="-")
+    print ("+", end="\n")   
+
+    for i in range(len(tabuleiro)):
+        print (i,"|", end=" ")
+        for j in range(len(tabuleiro[i])):
+            print(tabuleiro[i][j], end=" ")
+        print ("|")
+    print ("  ", end="")
+    print ("+", end="-")
+    for y in range(len(tabuleiro[0])):
+        print ("-", end="-")
+    print ("+", end="\n")
+    print()
+
+def trocar (linha1, coluna1, linha2, coluna2, tabuleiro):
+    ''' (int,int,int,int,matrix) -> Bool
+
+    Permuta gemas das posições (linha1, coluna1) e (linha2, coluna2) caso
+    seja válida (isto é, gemas são adjacentes e geram cadeias), caso contrário
+    não altera o tabuleiro.
+
+    Retorna `True` se permutação é válida e `False` caso contrário.
+    '''
+    v = identificar_cadeias_verticais (tabuleiro) 
+    h = identificar_cadeias_horizontais (tabuleiro)
+
+    cond = False
+    c = False
+    
+
+    if linha1 < len(tabuleiro) and linha1 == linha2:
+        if coluna1 == coluna2+1 or coluna1 == coluna2-1:
+            c = True
+    if coluna1 < len(tabuleiro[linha1]) and coluna1 == coluna2:
+        if linha1 == linha2+1 or linha1 == linha2-1:
+            c = True
+
+    if linha2 < len(tabuleiro) and coluna2 < len(tabuleiro[linha2])and c == True:
+        
+        aux1 = tabuleiro[linha1][coluna1]
+        aux2 = tabuleiro[linha2][coluna2]
+        tabuleiro[linha1][coluna1] = aux2
+        tabuleiro[linha2][coluna2] = aux1
+        v = identificar_cadeias_verticais (tabuleiro)
+        h = identificar_cadeias_horizontais (tabuleiro)
+        if len(v) > 0 or len(h) > 0:
+            cond = True
+        else:
+            tabuleiro[linha2][coluna2] = aux2
+            tabuleiro[linha1][coluna1] = aux1
+        
+
+        return cond
+
+
+def eliminar (tabuleiro):
+    ''' (matrix) -> int
+
+    Elimina cadeias de 3 ou mais gemas, substituindo-as por espaços (' ').
+
+    Retorna número de gemas eliminadas.
+    '''
+    num_eliminados = 0
+    h = identificar_cadeias_horizontais(tabuleiro)
+    v = identificar_cadeias_verticais(tabuleiro)
+
+    if len(h) > 0:
+        for x in range(len(h)):
+            z = eliminar_cadeia(tabuleiro, h[x])
+            num_eliminados += z
+    if len(v) > 0:
+        for y in range(len(v)):
+            w = eliminar_cadeia(tabuleiro, v[y])
+            num_eliminados += w
+    
+
+    return num_eliminados
+
+
+def identificar_cadeias_horizontais (tabuleiro):
+    ''' (matrix) -> list
+
+    Retorna uma lista contendo cadeias horizontais de 3 ou mais gemas. Cada cadeia é
+    representada por uma lista `[linha, coluna_i, linha, coluna_f]`, onde:
+
+    - `linha`: o número da linha da cadeia
+    - `coluna_i`: o número da coluna da gema mais à esquerda (menor) da cadeia
+    - `coluna_f`: o número da coluna da gema mais à direita (maior) da cadeia
+
+    Não modifica o tabuleiro.
+    '''
+    cadeias_horizontais = []
+
+    for i in range(len(tabuleiro)):
+        n = 1
+        j = 0
+        while j < len(tabuleiro[i])-1:
+            while n+j <= (len(tabuleiro[i])-1) and tabuleiro[i][j] == tabuleiro[i][j+n]:
+                if tabuleiro[i][j] == tabuleiro[i][j+n]:
+                    n += 1
+            if n >= 3:    
+                cadeias_horizontais.append([i, j, i, j+n-1])
+                j += n-1
+            n = 1
+            j += 1
+            
+            
+    return cadeias_horizontais
+
+def identificar_cadeias_verticais (tabuleiro):
+    ''' (matrix) -> list
+
+    Retorna uma lista contendo cadeias verticais de 3 ou mais gemas. Cada cadeia é
+    representada por uma lista `[linha_i, coluna, linha_f, coluna]`, onde:
+
+    - `linha_i`: o número da linha da gemas mais superior (menor) da cadeia
+    - `coluna`: o número da coluna das gemas da cadeia
+    - `linha_f`: o número da linha mais inferior (maior) da cadeia
+
+    Não modifica o tabuleiro.
+    '''
+    cadeias_verticais = []
+    i = 0
+
+    for j in range(len(tabuleiro[i])):
+        n = 1
+        while i < len(tabuleiro):
+            while n+i <= (len(tabuleiro)-1) and tabuleiro[i][j] == tabuleiro[i+n][j]:
+                if tabuleiro[i][j] == tabuleiro[i+n][j]:
+                    n += 1
+            if n >= 3:    
+                cadeias_verticais.append([i, j, i+n-1, j])
+                i += n-1
+            n = 1
+            i += 1
+        i = 0
+            
+
+    return cadeias_verticais
+
+def eliminar_cadeia (tabuleiro, cadeia):
+    ''' (matrix,list) -> int
+
+    Elimina (substitui pela string espaço `" "`) as gemas compreendidas numa cadeia,
+    representada por uma lista `[linha_inicio, coluna_inicio, linha_fim, coluna_fim]`,
+    tal que:
+
+    - `linha_i`: o número da linha da gema mais superior (menor) da cadeia
+    - `coluna_i`: o número da coluna da gema mais à esquerda (menor) da cadeia
+    - `linha_f`: o número da linha mais inferior (maior) da cadeia
+    - `coluna_f`: o número da coluna da gema mais à direita (maior) da cadeia
+
+    Retorna o número de gemas eliminadas.
+    '''
+    num_eliminados = 0
+    
+    if cadeia[0] == cadeia[2]:
+        coluna_i = cadeia[1]
+        coluna_f = cadeia[3]
+        linha = cadeia[0]
+        for j in range(coluna_i,coluna_f+1):
+            if tabuleiro[linha][j] != ' ':
+                tabuleiro[linha][j] = ' '
+                num_eliminados += 1
+
+    if cadeia[1] == cadeia[3]:
+        linha_i = cadeia[0]
+        linha_f = cadeia[2]
+        coluna = cadeia[3]
+        for i in range(linha_i,linha_f+1):
+            if tabuleiro[i][coluna] != ' ':
+                tabuleiro[i][coluna] = ' '
+                num_eliminados += 1
+            
+    
+    return num_eliminados
+
+
+def deslocar (tabuleiro):
+    ''' (matrix) -> None
+
+    Desloca gemas para baixo deixando apenas espaços vazios sem nenhuma gema acima.
+    '''
+
+    for j in range(len(tabuleiro[0])):
+        deslocar_coluna(tabuleiro, j)
+        
+        
+def deslocar_coluna (tabuleiro, j):
+    ''' (matrix, int) -> None
+
+    Desloca as gemas na coluna j para baixo, ocupando espaços vazios.
+    '''
+
+    x = len(tabuleiro)-1
+    while x > 0:
+        if tabuleiro[x][j] != " ":
+            x -= 1
+        else:
+            i = x
+            while i > 0:
+                tabuleiro[i][j] = tabuleiro[i-1][j]
+                i -= 1
+            tabuleiro[0][j] = " "
+        x -= 1
+                    
+
+def existem_movimentos_validos (tabuleiro):
+    '''(matrix) -> Bool
+
+    Retorna True se houver movimentos válidos, False caso contrário.
+    '''
+
+    cond = False
+
+    if obter_dica != -1:
+        cond = True
+
+    return cond
+
+
+def obter_dica (tabuleiro):
+    '''(matrix) -> int,int
+
+    Retorna a posição (linha, coluna) de uma gema que faz parte de uma
+    permutação válida.
+
+    Se não houver permutação válida, retorne -1,-1.
+    '''
+
+    linha = -1
+    coluna = -1
+    i = 0
+    j = 0
+    
+    for i in range(len(tabuleiro)):
+        k = i+1
+        for j in range(len(tabuleiro[i])):
+            l = j+1
+            if i < len(tabuleiro) and k < len(tabuleiro) :
+                if trocar(i, j, k, j, tabuleiro) == True:
+                    linha = i
+                    coluna = j
+                    aux1 = tabuleiro[i][j]
+                    aux2 = tabuleiro[k][j]
+                    tabuleiro[i][j] = aux2
+                    tabuleiro[k][j] = aux1
+            if j < len(tabuleiro[i]) and l < len(tabuleiro[i]):
+                if trocar(i, j, i, l, tabuleiro) == True:
+                    linha = i
+                    coluna = j
+                    aux1 = tabuleiro[i][j]
+                    aux2 = tabuleiro[i][l]
+                    tabuleiro[i][j] = aux2
+                    tabuleiro[i][l] = aux1
+            
+                    
+    return linha, coluna
+
+
+main()
+
